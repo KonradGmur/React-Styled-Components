f1805523377f9a5d7f87017114c18921ad33641d
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-24 14:18:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import "./styles.css";
 
 const Button = styled.button`
-  background: ${props => (props.secondary ? "white" : "red")};
+  background: ${props =>
+    props.secondary ? props.theme.colors.secondary : props.theme.colors.main};
   color: ${props => (props.secondary ? "red" : "white")};
 
   font-size: 1rem;
@@ -14,16 +15,30 @@ const Button = styled.button`
   cursor: pointer;
 
   &:hover {
-    transform: scale(1.5);
+    color: #000;
   }
 `;
 
+const InlineButton = styled(Button)`
+  background: unset;
+  color: #000;
+`;
+const theme = {
+  colors: {
+    main: "red",
+    secondary: "white"
+  }
+};
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>React- styled components</h1>
-      <Button>Przycisk</Button>
-      <Button secondary>Przycisk</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>React- styled components</h1>
+        <Button>Przycisk</Button>
+        <Button secondary>Przycisk</Button>
+        <InlineButton>Infline button</InlineButton>
+      </div>
+    </ThemeProvider>
   );
 }
