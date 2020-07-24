f6e60c7937b6f38d3735f8af28f3253a5a99bb2d
Konrad <57108310+KonradGmur@users.noreply.github.com>
2020-07-24 14:09:23 +0200

    Creating sample buttons and using props in styles components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import "./styles.css";
+
+const Button = styled.button`
+  background: ${props => (props.secondary ? "white" : "red")};
+  color: ${props => (props.secondary ? "red" : "white")};
+
+  font-size: 1rem;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid blue;
+  border-radius: 3px;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
 
 export default function App() {
   return (
     <div className="App">
       <h1>React- styled components</h1>
+      <Button>Przycisk</Button>
+      <Button secondary>Przycisk</Button>
     </div>
   );
 }
